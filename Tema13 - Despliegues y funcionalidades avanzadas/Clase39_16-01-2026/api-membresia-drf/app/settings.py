afc921e71ce18a61d6d4ef8dc4100d7a6f709e38
Jhonathan <jhernandez02@gmail.com>
2026-01-16 23:29:24 -0500

    Clase39

--- Tema13 - Despliegues y funcionalidades avanzadas/Clase39_16-01-2026/api-membresia-drf/app/settings.py
+++ Tema13 - Despliegues y funcionalidades avanzadas/Clase39_16-01-2026/api-membresia-drf/app/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 from dotenv import load_dotenv
+import os
+import dj_database_url
 
 load_dotenv()
 
@@ -82,10 +84,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default=os.getenv('DB_URL'),
+        conn_max_age=600
+    )
 }
 
 
@@ -131,7 +134,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    os.getenv('FRONT_URL'),
 ]
 
 REST_FRAMEWORK = {
